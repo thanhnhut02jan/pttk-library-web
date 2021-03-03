f44f40a21aa92c7bbd51a9e9b365d52611ef19fd
thanhnhut02jan <thanhnhut.02jan@gmail.com>
2021-03-03 12:47:53 +0700

    use material ui instead bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-
 import {
   BrowserRouter as Router,
   Switch,
@@ -7,26 +5,42 @@ import {
   Link
 } from "react-router-dom";
 
-import TopNav from './components/TopNav';
-import Home from './pages/home';
-import QuanLySach from './pages/QuanLySach'
+import React from 'react'
+
+
+import Container from "@material-ui/core/Container";
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Button from '@material-ui/core/Button';
 
 function App() {
   return (
     <Router>
 
-      <TopNav />
+      <React.Fragment>
+        <CssBaseline />
+        <AppBar position="relative">
+          <Toolbar>
+            <Typography variant="h6" color="inherit" noWrap>
+              Library
+          </Typography>
+          </Toolbar>
+        </AppBar>
 
-      <Switch>
-        <Route exact path='/'>
-          <Home />
-        </Route>
+        <Container>
+          <Switch>
+            <Route exact path='/'>
 
-        <Route exact path='/books-management/'>
-          <QuanLySach />
-        </Route>
-      </Switch>
+            </Route>
 
+            <Route exact path='/books-management/'>
+
+            </Route>
+          </Switch>
+        </Container>
+      </React.Fragment>
     </Router>
 
   );
